bd3828ed41a90e61d5ee2ffacf1a0619050d2288
ChapaNoob <128272186+ChapaNoob@users.noreply.github.com>
2025-04-01 14:54:40 +0000

    Criei o começo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
@@ -19,4 +20,87 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main() {
+    char estado [50];
+    char codcarta [] = "A01";
+    char nomecidade [] = "são paulo";
+    int populacao;
+    int pontosturi;
+    float area;
+    float pib;
+
+    char estado2 [50];
+    char codcarta2 [] = "A01";
+    char nomecidade2 [] = "são paulo";
+    int populacao2;
+    int pontosturi2;
+    float area2;
+    float pib2;
+
+
+    printf("Digite o nome do estado: ");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo da carta: ");
+    scanf("%s", &codcarta);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &nomecidade);
+
+    printf("Digite o numero da população: ");
+    scanf("%d", &populacao);  
+
+    printf("Digite a Área em km²: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &pib);
+
+    printf("Digite quantos pontos turisticos tem a cidade: ");
+    scanf("%d", &pontosturi);
+
+    
+   
+    printf("Agora informe sobre a segunda carta \n");
+
+    
+    printf("Digite o nome do estado: ");
+    scanf("%s", &estado2);
+
+    printf("Digite o codigo da carta: ");
+    scanf("%s", &codcarta2);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &nomecidade2);
+
+    printf("Digite o numero da população: ");
+    scanf("%d", &populacao2);  
+
+    printf("Digite a Área em km²: ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &pib2);
+
+    printf("Digite quantos pontos turisticos tem a cidade: ");
+    scanf("%d", &pontosturi2);  
+
+
+
+    printf("Carta 1: \n");
+    printf("Estado: %s \n Codigo: %s \n Nome da Cidade: %s \n População: %d \n Área: %f \n PIB: %f \n Numero de Pontos Turisticos: %d \n", estado, codcarta, nomecidade, populacao, area, pib, pontosturi);
+
+    printf("Carta 2: \n");
+    printf("Estado: %s \n Codigo: %s \n Nome da Cidade: %s \n População: %d \n Área: %f \n PIB: %f \n Numero de Pontos Turisticos: %d ", estado2, codcarta2, nomecidade2, populacao2, area2, pib2, pontosturi2);
+
+
+
+
+
+
 }
